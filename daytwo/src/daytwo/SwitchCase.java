32cf9a88f37c6b5d7891e6f3b395ab486b2c47f7
Anjali <Anjali@LAPTOP-9CU206FI>
2025-07-02 10:00:12 +0530

    create switch case

---
+++ daytwo/src/daytwo/SwitchCase.java
@@ -0,0 +1,19 @@
+package daytwo;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day = 2;
+		switch (day) {
+		    case 1:
+		        System.out.println("Monday");
+		        break;
+		    case 2:
+		        System.out.println("Tuesday");
+		        break;
+		    default:
+		        System.out.println("Another day");
+		}
+
+		}
+}
